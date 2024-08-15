14e590fbfdcdddd87335632ecff21ba9231ab2e8
James Lee <leejungmin122@gmail.com>
2024-08-15 15:20:20 +0900

    블로그 타이틀 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'My Blog',
+  title: 'DawnScapeLab',
   description: 'A personal blog built with Next.js',
 }
 
