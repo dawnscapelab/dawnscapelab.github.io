6c018c01958ad3c8e4ab72aad2f16f69704824e9
James Lee <leejungmin122@gmail.com>
2024-08-25 09:53:05 +0900

    software-development 카테고리 아이콘 추가

--- components/CategoryIcon.tsx
+++ components/CategoryIcon.tsx
@@ -1,4 +1,4 @@
-import { FaAndroid, FaReact, FaFolder, FaInfinity } from 'react-icons/fa';
+import { FaAndroid, FaReact, FaFolder, FaInfinity, FaCode } from 'react-icons/fa';
 import { SiGradle } from 'react-icons/si';
 
 const categoryIcons: { [key: string]: JSX.Element } = {
@@ -6,6 +6,7 @@ const categoryIcons: { [key: string]: JSX.Element } = {
     gradle: <SiGradle className="h-10 w-10 text-indigo-600" />,
     nextjs: <FaReact className="h-10 w-10 text-indigo-600" />, // Next.js 카테고리를 위한 React 아이콘 추가
     devops: <FaInfinity className="h-10 w-10 text-indigo-600" />, // DevOps 카테고리를 위한 무한대 아이콘 추가
+    'software-development': <FaCode className="h-10 w-10 text-indigo-600" />, // 소프트웨어 개발 카테고리를 위한 코드 아이콘 추가
 };
 
 export default function CategoryIcon({ category }: { category: string }) {
